85689560a0c670de9c5f893f980b7a18c1ed0129
John Bae <john21225@gmail.com>
2022-01-22 19:11:04 -0800

    Adding onHover event listener

--- app/src/components/ColorLegend.js
+++ app/src/components/ColorLegend.js
@@ -5,9 +5,15 @@ const ColorLegend = ({
 	legendSpacing = 20,
 	legendCircleRadius = 10,
 	legendTextOffset = 20,
+	onHover = (hoveredValue) => {
+		console.log(hoveredValue);
+	},
 }) =>
 	colorScale.domain().map((domainValue, i) => (
-		<g transform={`translate(0, ${i * legendSpacing})`}>
+		<g
+			transform={`translate(0, ${i * legendSpacing})`}
+			onMouseEnter={() => onHover(domainValue)}
+		>
 			<circle fill={colorScale(domainValue)} r={legendCircleRadius} />
 			<text className="legend-text" x={legendTextOffset} dy=".32em">
 				{domainValue}
